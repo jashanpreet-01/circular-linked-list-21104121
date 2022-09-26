8a86ee565a70886b57511732a8f725c1d414d5a1
jashanpreet-01 <97078781+jashanpreet-01@users.noreply.github.com>
2022-09-26 23:35:46 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,10 @@
+Question 1: While traversing a single-circular linked list, which condition establishes that the traversing  
+element /variable has reached the first element?
+
+Answer:
+
+ A pointer to the list's first node is found at the list's last node in a circular singly linked list. When we reach the first node once more in a 
+ circular linked list, the traversal is stopped. The last item's next pointer in a singly linked list points to the first item. A singly linked list
+ only has one pointer that leads to NULL, which is the pointer from the list's final node. The 'Next' pointer of the last node must point to the first 
+ node if we want the singly linked list to be circular. The last node of the list must be present for the traversing element to have reached the first 
+ element.
